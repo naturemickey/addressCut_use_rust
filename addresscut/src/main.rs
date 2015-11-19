da963aa5f30208f50ec4f06943d3eac56a91215a
zhouyou <mickey_zhouyou@yeah.net>
2015-11-20 07:40:56 +0800

    commitall

--- addresscut/src/main.rs
+++ addresscut/src/main.rs
@@ -1,8 +1,13 @@
 
 extern crate addresscut;
 
-static N:i8 = 5;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
+	let mut f = try!(File::open("citybasedata.config"));
+	let mut s = String::new();
+	try!(f.read_to_string(&mut s));
+	println!("{}", s);
 	println!("address cut");
 }
