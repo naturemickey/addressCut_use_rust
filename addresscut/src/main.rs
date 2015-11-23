6f83f5a83f71cf017aaba5e82abf54b2e149b9f5
Michael Chow <mickey_zhouyou@yeah.net>
2015-11-23 11:17:14 +0800

    commitall

--- addresscut/src/main.rs
+++ addresscut/src/main.rs
@@ -4,6 +4,15 @@ extern crate addresscut;
 use std::fs::File;
 use std::io::prelude::*;
 
+macro_rules! try1 {
+    ($expr:expr) => (match $expr {
+        $crate::result::Result::Ok(val) => val,
+        $crate::result::Result::Err(err) => {
+            return $crate::result::Result::Err($crate::convert::From::from(err))
+        }
+    })
+}
+
 fn main() {
 	let mut f = try!(File::open("citybasedata.config"));
 	let mut s = String::new();
