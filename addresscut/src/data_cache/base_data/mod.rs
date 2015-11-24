60d9dc3a91f6f4751c6a2f95759be57bb1a8ac12
Michael Chow <mickey_zhouyou@yeah.net>
2015-11-24 13:37:54 +0800

    commitall

--- addresscut/src/data_cache/base_data/mod.rs
+++ addresscut/src/data_cache/base_data/mod.rs
@@ -1,14 +1,14 @@
 
-
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use std::option::Option::{None, Some};
+use std::str::FromStr;
 
 pub struct City {
-	id:i16,
-	pid:i16,
+	id:i32,
+	pid:i32,
 	lvl:i8,
 	names:Vec<String>
 }
@@ -38,16 +38,31 @@ pub fn all_citys() -> Vec<City> {
 	let lines:Vec<&str> = s.lines().collect();
 	for line in lines {
 		let ss:Vec<&str> = line.split(',').collect();
-		// for s in ss {
-		// 	println!("{}", s);
-		// }
-		for i in 0..ss.len() {
+		let mut _id:i32;
+		let mut _pid:i32;
+		let mut _lvl:i8;
+		let mut _names:Vec<String> = Vec::new();
+		for i in 0 .. ss.len() {
 			match ss.get(i) {
 				None => println!("error"),
-				Some(s) => println!("{}", s)
+				Some(s) => {
+					match i {
+						0 => _id = str_to_int::<i32>(s),
+						1 => _pid = str_to_int::<i32>(s),
+						2 => _lvl = str_to_int::<i8>(s),
+						_ => _names.push(s.to_string()),
+					}
+				}
 			}
 		}
 	}
 
 	citys
+}
+
+fn str_to_int<T: FromStr>(s: &str) -> T {
+	match T::from_str(s) {
+		Err(why) => panic!("couldn't convert {}", s),
+		Ok(_i) => _i,
+	}
 }
