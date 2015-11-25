330e22d7e0329fa1d63ed16671f616ba0d01a763
zhouyou <mickey_zhouyou@yeah.net>
2015-11-25 20:35:13 +0800

    commitall

--- addresscut/src/data_cache/mod.rs
+++ addresscut/src/data_cache/mod.rs
@@ -7,7 +7,7 @@ use std::option::Option;
 
 pub fn im() {
 	let ac = base_data::all_citys();
-	let mut nameMap:HashMap<String, Vec<i32> > = HashMap::new();
+	let mut nameMap = HashMap::new();
 	for c in &ac {
 		for name in &c.names {
 			nameMap.entry(name.to_string()).or_insert(Vec::new()).push(c.id);
