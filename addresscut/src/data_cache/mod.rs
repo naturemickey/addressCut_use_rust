501e323b6b786cf664ee40706926ef0bc9860952
Michael Chow <mickey_zhouyou@yeah.net>
2015-11-25 17:51:41 +0800

    commitall

--- addresscut/src/data_cache/mod.rs
+++ addresscut/src/data_cache/mod.rs
@@ -5,25 +5,12 @@ use std::collections::HashMap;
 use std::option::Option::{None, Some};
 use std::option::Option;
 
-fn insert_to_name_map(nameMap:&mut HashMap<String, Vec<i32> >, name:&str, id:i32) {
-	match nameMap.get_mut(name) {
-		None => {
-			let mut v = Vec::new();
-			v.push(id);
-			nameMap.insert(name.to_string(), v);
-		}
-		Some(v) => {
-			v.push(id);
-		}
-	}
-}
-
 pub fn im() {
 	let ac = base_data::all_citys();
 	let mut nameMap:HashMap<String, Vec<i32> > = HashMap::new();
 	for c in &ac {
 		for name in &c.names {
-			insert_to_name_map(&mut nameMap, name, c.id);
+			nameMap.entry(name.to_string()).or_insert(Vec::new()).push(c.id);
 		}
 	}
 }
