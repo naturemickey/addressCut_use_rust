b9a637864f43c8a36f9018d0e161cf7abe22048b
Michael Chow <mickey_zhouyou@yeah.net>
2015-12-02 17:23:01 +0800

    commitall

--- addresscut/src/main.rs
+++ addresscut/src/main.rs
@@ -1,12 +1,21 @@
 
 extern crate addresscut;
+extern crate time;
 
 use addresscut::AddressScanner;
 use addresscut::Address;
+use std::ops::Sub;
+
 
 fn main() {
+    let tm1 = time::now();
     let scanner = AddressScanner::new();
-    let address = scanner.scan("四川遂宁市湖北黄冈市蕲春县湖北省黄冈市蕲春县林业局");
+    let tm2 = time::now();
+    println!("construct scanner use {} ms.", (tm2.sub(tm1).num_milliseconds()));
+    let address = scanner.scan("山东省莱芜市莱城区吕花园美食街，品品香茶业");
+    let tm3 = time::now();
+    println!("scan an address use {} ms.", (tm3.sub(tm2).num_milliseconds()));
+    
     println!("province_address : {}", &address.province_address);
     println!("city_address     : {}", &address.city_address);
     println!("area_address     : {}", &address.area_address);
