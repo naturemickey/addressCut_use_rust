8cb10976c00ab37983775cbeb5570f07c048abcb
Michael Chow <mickey_zhouyou@yeah.net>
2018-01-06 18:00:55 +0800

    remove dependences

--- addresscut/src/main.rs
+++ addresscut/src/main.rs
@@ -1,6 +1,5 @@
 
 extern crate addresscut;
-extern crate time;
 
 use std::error::Error;
 use std::fs::File;
@@ -10,16 +9,17 @@ use addresscut::AddressScanner;
 use addresscut::Address;
 use std::ops::Sub;
 use std::result::Result::{Ok, Err};
+use std::time::{Duration, Instant};
 
 
 fn main() {
-    let tm1 = time::now();
+    let tm1 = Instant::now();
     let scanner = AddressScanner::new();
-    let tm2 = time::now();
-    println!("construct scanner use {} ms.", (tm2.sub(tm1).num_milliseconds()));
+    let tm2 = Instant::now();
+    println!("construct scanner use {:?}.", tm2.duration_since(tm1));
     let address = scanner.scan("江西抚州市南昌大学抚州医学分院12级全科2班");
-    let tm3 = time::now();
-    println!("scan an address use {} ms.", (tm3.sub(tm2).num_milliseconds()));
+    let tm3 = Instant::now();
+    println!("scan an address use {:?}.", (tm3.duration_since(tm2)));
 
     println!("province_address : {}", &address.province_address);
     println!("city_address     : {}", &address.city_address);
@@ -29,7 +29,7 @@ fn main() {
     println!("detail_address   : {}", &address.detail_address);
 
 
-    let tm4 = time::now();
+    let tm4 = Instant::now();
     let path = Path::new("测试地址.txt");
     let display = path.display();
 
@@ -45,16 +45,16 @@ fn main() {
     }
 
 	let lines:Vec<&str> = s.lines().collect();
-    let tm5 = time::now();
-    println!("读取所有测试地址消耗时长 {} ms.", (tm5.sub(tm4).num_milliseconds()));
+    let tm5 = Instant::now();
+    println!("读取所有测试地址消耗时长 {:?}.", (tm5.duration_since(tm4)));
     
     let mut addr_list = Vec::with_capacity(250000);
     for i in 0..lines.len() { unsafe {
         // println!("{}", s);
         addr_list.push(scanner.scan(lines.get_unchecked(i)));
     }}
-    let tm6 = time::now();
-    println!("识别所有测试地址消耗时长 {} ms.", (tm6.sub(tm5).num_milliseconds()));
+    let tm6 = Instant::now();
+    println!("识别所有测试地址消耗时长 {:?}.", (tm6.duration_since(tm5)));
     
     // write_file(addr_list, &lines);
     // let tm7 = time::now();
